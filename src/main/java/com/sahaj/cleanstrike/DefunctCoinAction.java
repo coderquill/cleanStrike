249915e14ea0f9af5768efe3ad4b187a125f51fd
Rupali Kavale <rupali.kavale@groww.in>
2021-03-12 19:51:53 +0530

    Add defunct coin action

---
+++ src/main/java/com/sahaj/cleanstrike/DefunctCoinAction.java
@@ -0,0 +1,25 @@
+package com.sahaj.cleanstrike;
+
+public class DefunctCoinAction implements GameAction {
+    @Override
+    public Board executeOnBoard(Board board) {
+        if (board.getBlackCoinCount() >= 1)
+            board.removeBlackCoins(1);
+        else {
+            board.removeRedCoin();
+        }
+        return board;
+    }
+
+    @Override
+    public Player executeOnPlayer(Player player) {
+        player.decrementScore(2);
+        return player;
+    }
+
+    @Override
+    public boolean canExecute(Board board, Player player) {
+        return board.getTotalCoinCount() >= 1;
+    }
+
+}
---
+++ src/test/java/com/sahaj/cleanstrike/DefunctCoinActionTest.java
@@ -0,0 +1,63 @@
+package com.sahaj.cleanstrike;
+
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DefunctCoinActionTest {
+
+    @Test
+    public void testShouldRemoveBlackCoinWhenBlackCoinIsPresent() throws InvalidBoardException {
+        GameAction defunctCoinAction = new DefunctCoinAction();
+        Player player = Player.initializePlayer("testPlayer");
+        Board board = Board.buildBoard(0, 1);
+
+        assertTrue(defunctCoinAction.canExecute(board, player));
+        defunctCoinAction.execute(board, player);
+
+        assertEquals(board.getBlackCoinCount(), 0);
+        assertEquals(board.getRedCoinCount(), 0);
+        assertEquals(player.getScore(), -2);
+
+
+        Player player1 = Player.initializePlayer("testPlayer");
+        Board board1 = Board.buildBoard(1, 1);
+
+        assertTrue(defunctCoinAction.canExecute(board1, player1));
+        defunctCoinAction.execute(board1, player1);
+
+        assertEquals(board1.getBlackCoinCount(), 0);
+        assertEquals(board1.getRedCoinCount(), 1);
+        assertEquals(player1.getScore(), -2);
+
+    }
+
+    @Test
+    public void testShouldRemoveRedCoinWhenNoBlackCoinAvailable() throws InvalidBoardException {
+        GameAction defunctCoin = new DefunctCoinAction();
+        Board board = Board.buildBoard(1, 0);
+        Player player = Player.initializePlayer("testPlayer");
+
+        assertTrue(defunctCoin.canExecute(board, player));
+        defunctCoin.execute(board, player);
+
+        assertEquals(board.getRedCoinCount(), 0);
+        assertEquals(board.getBlackCoinCount(), 0);
+
+    }
+
+    @Test
+    public void testShouldDecrementScoreByTwoForDefunctAction() throws InvalidBoardException {
+        GameAction defunctCoin = new DefunctCoinAction();
+        Board board = Board.buildDefaultBoard();
+        Player player = Player.initializePlayer("testPlayer");
+
+        assertTrue(defunctCoin.canExecute(board, player));
+        defunctCoin.execute(board, player);
+
+        assertEquals(player.getScore(), -2);
+    }
+
+}
