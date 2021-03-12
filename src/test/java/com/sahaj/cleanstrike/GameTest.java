ff1bdf65328ecb2458b25c6f76f70a8fa0da41ce
Rupali Kavale <rupali.kavale@groww.in>
2021-03-13 01:57:05 +0530

    Add tests for last three consecutive fouls scenario

--- src/test/java/com/sahaj/cleanstrike/GameTest.java
+++ src/test/java/com/sahaj/cleanstrike/GameTest.java
@@ -12,7 +12,8 @@ public class GameTest  {
 
     @Test
     public void testGameExecutionWhenMultipleSingeStrikeActionsOccur() throws InvalidBoardException {
-        List<GameAction> gameActionSequence = Arrays.asList(new StrikeAction(),
+        List<GameAction> gameActionSequence = Arrays.asList(
+                new StrikeAction(),
                 new StrikeAction(),
                 new StrikeAction(),
                 new StrikeAction(),
@@ -29,7 +30,8 @@ public class GameTest  {
     }
     @Test
     public void testGameExecutionForGivenGameActionSequence() throws InvalidBoardException {
-        List<GameAction> gameActionSequence = Arrays.asList(new StrikerStrikeAction(),
+        List<GameAction> gameActionSequence = Arrays.asList(
+                new StrikerStrikeAction(),
                 new StrikeAction(),
                 new MultiStrikeAction(),
                 new RedStrikeAction(),
@@ -41,6 +43,76 @@ public class GameTest  {
         Player player = Player.initializePlayer("Player1");
         Game.executeGamePlay(Board.buildDefaultBoard(), player,
                 gameActionSequence);
+        Assert.assertEquals(player.getScore(), 3);
+    }
+
+    @Test
+    public void testGameExecutionForActionSequenceWithFoulActions() throws InvalidBoardException {
+        List<GameAction> gameActionSequence = Arrays.asList(
+                new StrikerStrikeAction(),
+                new StrikeAction(),
+                new MultiStrikeAction(),
+                new RedStrikeAction(),
+                new DefunctCoinAction(),
+                new DefunctCoinAction(),
+                new StrikeAction(),
+                new MultiStrikeAction(),
+                new StrikeAction(),
+                new StrikeAction(),
+                new DefunctCoinAction());
+
+        Player player = Player.initializePlayer("Player1");
+        Game.executeGamePlay(Board.buildDefaultBoard(), player,
+                gameActionSequence);
         Assert.assertEquals(player.getScore(), 4);
+    }
+
+    @Test
+    public void testGameExecutionForOnlyFoulActions() throws InvalidBoardException {
+        List<GameAction> gameActionSequence = Arrays.asList(
+                new StrikerStrikeAction(),
+                new DefunctCoinAction(),
+                new DefunctCoinAction(),
+                new StrikerStrikeAction(),
+                new DefunctCoinAction());
+
+        Player player = Player.initializePlayer("Player1");
+        Game.executeGamePlay(Board.buildDefaultBoard(), player,
+                gameActionSequence);
+        Assert.assertEquals(player.getScore(), -12);
+    }
+
+    @Test
+    public void testGameExecutionForSuccessiveThreeFaultyActions() throws InvalidBoardException {
+        List<GameAction> gameActionSequence = Arrays.asList(
+                new StrikeAction(),
+                new MultiStrikeAction(),
+                new DefunctCoinAction(),
+                new StrikerStrikeAction(),
+                new DefunctCoinAction(),
+                new RedStrikeAction()
+        );
+
+        Player player = Player.initializePlayer("Player1");
+        Game.executeGamePlay(Board.buildDefaultBoard(), player,
+                gameActionSequence);
+        Assert.assertEquals(player.getScore(), -1);
+    }
+
+    @Test
+    public void testGameExecutionForOnlyDefunctCoinAction() throws InvalidBoardException {
+        List<GameAction> gameActionSequence = Arrays.asList(
+                new DefunctCoinAction(),
+                new DefunctCoinAction(),
+                new DefunctCoinAction(),
+                new DefunctCoinAction(),
+                new DefunctCoinAction(),
+                new DefunctCoinAction()
+        );
+
+        Player player = Player.initializePlayer("Player1");
+        Game.executeGamePlay(Board.buildDefaultBoard(), player,
+                gameActionSequence);
+        Assert.assertEquals(player.getScore(), -18);
     }
 }
