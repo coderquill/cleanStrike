1c2211aea9c6e11e69c1a3dc87b8f150e47fc861
Rupali Kavale <rupali.kavale@groww.in>
2021-03-12 21:38:03 +0530

    Track player fouls

--- src/main/java/com/sahaj/cleanstrike/Player.java
+++ src/main/java/com/sahaj/cleanstrike/Player.java
@@ -4,31 +4,56 @@ public class Player {
 
     private String name;
     private int score;
+    private int foulCount;
 
-    Player(String name, int score){
+    Player(String name, int score) {
         this.name = name;
         this.score = score;
+        this.foulCount = 0;
     }
 
-    public String getName(){
+    public String getName() {
         return this.name;
     }
 
-    public int getScore(){
+    public int getScore() {
         return this.score;
     }
 
-    public void incrementScore(int scoreToBeAdded){
+    public void incrementScore(int scoreToBeAdded) {
         int currentScore = this.getScore();
         this.score = currentScore + scoreToBeAdded;
     }
 
-    public void decrementScore(int scoreToBeSubtracted){
+    public void decrementScore(int scoreToBeSubtracted) {
         int currentScore = this.getScore();
         this.score = currentScore - scoreToBeSubtracted;
     }
 
-    public static Player initializePlayer(String name){
+    public int getFoulCount() {
+        return this.foulCount;
+    }
+
+    public void handleFoul() {
+        int currentFoulCount = this.getFoulCount();
+        if(currentFoulCount == 2){
+            resetFoulCount();
+            decrementScore(1);
+        }else if( currentFoulCount < 3){
+            addFoul();
+        }
+    }
+
+    public void resetFoulCount(){
+        this.foulCount = 0;
+    }
+
+
+    public void addFoul(){
+        this.foulCount = this.getFoulCount()+1;
+    }
+
+    public static Player initializePlayer(String name) {
         int defaultScore = 0;
         return new Player(name, defaultScore);
     }
--- src/test/java/com/sahaj/cleanstrike/PlayerTest.java
+++ src/test/java/com/sahaj/cleanstrike/PlayerTest.java
@@ -16,6 +16,7 @@ public class PlayerTest extends TestCase {
       assertEquals(playerScore, 0);
   }
 
+  @Test
   public void testShouldGetPlayerName() {
     Player player1 = Player.initializePlayer("player1");
     String playerName = player1.getName();
@@ -23,6 +24,7 @@ public class PlayerTest extends TestCase {
     assertEquals(playerName, "player1");
   }
 
+  @Test
   public void testShouldGetPlayerScore() {
     Player player1 = Player.initializePlayer("player1");
     int playerScore = player1.getScore();
@@ -30,6 +32,7 @@ public class PlayerTest extends TestCase {
     assertEquals(playerScore, 0);
   }
 
+  @Test
   public void testShouldIncrementPlayerScoreByProvidedAmount() {
     Player player1 = Player.initializePlayer("player1");
     int initialPlayerScore = player1.getScore();
@@ -40,6 +43,47 @@ public class PlayerTest extends TestCase {
     player1.incrementScore(incrementScoreByTwo);
 
     assertEquals(player1.getScore(), initialPlayerScore+incrementScoreByONe+incrementScoreByTwo);
+  }
+
+  @Test
+  public void testShouldGetFoulCount(){
+    Player player = Player.initializePlayer("testPlayer");
+
+    assertEquals(player.getFoulCount(), 0);
+  }
+
+  @Test
+  public void testShouldAddFoul(){
+    Player player = Player.initializePlayer("testPlayer");
+    player.addFoul();
+    assertEquals(player.getFoulCount(), 1);
+
+    player.addFoul();
+    assertEquals(player.getFoulCount(), 2);
+
+  }
+
+  @Test
+  public void testShouldResetFoulCountAndDecrementScore(){
+    Player player = Player.initializePlayer("testPlayer");
+    player.handleFoul();
+    player.handleFoul();
+    player.handleFoul();
+
+
+    assertEquals(player.getFoulCount(), 0);
+    assertEquals(player.getScore(), -1);
+
+  }
+
+  @Test
+  public void testShouldAddFoulWhenFoulsLessThanThree(){
+    Player player = Player.initializePlayer("testPlayer");
+
+    player.handleFoul();
+
+    assertEquals(player.getFoulCount(), 1);
+    assertEquals(player.getScore(), 0);
   }
 
 }
