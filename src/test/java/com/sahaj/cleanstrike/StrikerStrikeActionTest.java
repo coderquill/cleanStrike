f9d46017bd3f60790440e7d6754d26cee5524aac
Rupali Kavale <rupali.kavale@groww.in>
2021-03-13 21:43:43 +0530

    Add tests for striker strike action

--- src/test/java/com/sahaj/cleanstrike/StrikerStrikeActionTest.java
+++ src/test/java/com/sahaj/cleanstrike/StrikerStrikeActionTest.java
@@ -1,6 +1,5 @@
 package com.sahaj.cleanstrike;
 
-import junit.framework.TestCase;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -9,22 +8,18 @@ import static org.junit.Assert.assertTrue;
 public class StrikerStrikeActionTest {
 
     @Test
-    public void testShouldExecuteGameActionStrikerStrike() throws InvalidBoardException {
+    public void testShouldDecrementScoreByOneForStrikerStrike() throws InvalidBoardException {
         GameAction strikerStrikeAction = new StrikerStrikeAction();
         Player player = Player.initializePlayer("testPlayer");
         Board board = Board.buildBoard(1, 6);
 
-        assertTrue(strikerStrikeAction.canExecute(board, player));
         strikerStrikeAction.execute(board, player);
 
-        assertEquals(board.getBlackCoinCount(), 6);
-        assertEquals(board.getRedCoinCount(), 1);
         assertEquals(player.getScore(), -1);
-        assertEquals(player.getFoulCount(), 1);
     }
 
     @Test
-    public void testShouldAddFoulForStrikerStrikeAction() throws InvalidBoardException {
+    public void testShouldAddFoulForStrikerStrike() throws InvalidBoardException {
         GameAction strikerStrikeAction = new StrikerStrikeAction();
         Board board = Board.buildDefaultBoard();
         Player player = Player.initializePlayer("testPalyer");
@@ -33,6 +28,30 @@ public class StrikerStrikeActionTest {
         strikerStrikeAction.execute(board, player);
 
         assertEquals(player.getFoulCount(), 1);
+    }
+
+    @Test
+    public void testShouldNotChangeBlackCoinCountForStrikerStrike() throws InvalidBoardException {
+        GameAction strikerStrikeAction = new StrikerStrikeAction();
+        Board board = Board.buildBoard(1, 6);
+        Player player = Player.initializePlayer("testPalyer");
+
+        assertTrue(strikerStrikeAction.canExecute(board, player));
+        strikerStrikeAction.execute(board, player);
+
+        assertEquals(board.getBlackCoinCount(), 6);
+    }
+
+    @Test
+    public void testShouldNotChangeRedCoinCountForStrikerStrike() throws InvalidBoardException {
+        GameAction strikerStrikeAction = new StrikerStrikeAction();
+        Board board = Board.buildBoard(2, 6);
+        Player player = Player.initializePlayer("testPalyer");
+
+        assertTrue(strikerStrikeAction.canExecute(board, player));
+        strikerStrikeAction.execute(board, player);
+
+        assertEquals(board.getRedCoinCount(), 2);
     }
 
 }
