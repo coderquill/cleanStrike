eb7d27e5197c5064d3b2f893be2df5d48b41ae9a
Rupali Kavale <rupali.kavale@groww.in>
2021-03-13 21:41:43 +0530

    Add tests for red strike action

--- src/test/java/com/sahaj/cleanstrike/RedStrikeActionTest.java
+++ src/test/java/com/sahaj/cleanstrike/RedStrikeActionTest.java
@@ -1,26 +1,10 @@
 package com.sahaj.cleanstrike;
 
-import junit.framework.TestCase;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class RedStrikeActionTest {
-
-    @Test
-    public void testShouldExecuteGameActionRedStrike() throws InvalidBoardException {
-
-        GameAction redStrike = new RedStrikeAction();
-        Player player = Player.initializePlayer("testPlayer");
-        Board board = Board.buildBoard(1, 9);
-
-        assertTrue(redStrike.canExecute(board, player));
-        redStrike.execute(board, player);
-
-        assertEquals(board.getBlackCoinCount(), 9);
-        assertEquals(board.getRedCoinCount(), 0);
-        assertEquals(player.getScore(), 3);
-    }
 
     @Test
     public void testShouldNotExecuteRedStrikeWhenNoRedCoinAvailable() throws InvalidBoardException {
@@ -29,6 +13,41 @@ public class RedStrikeActionTest {
         Player player = Player.initializePlayer("testPlayer");
 
         assertFalse(redStrike.canExecute(board, player));
+    }
+
+    @Test
+    public void testShouldExecuteRedStrikeWhenRedCoinAvailable() throws InvalidBoardException {
+        GameAction redStrike = new RedStrikeAction();
+        Board board = Board.buildBoard(1, 1);
+        Player player = Player.initializePlayer("testPlayer");
+
+        assertTrue(redStrike.canExecute(board, player));
+    }
+
+    @Test
+    public void testShouldRemoveRedCoinForRedStrike() throws InvalidBoardException {
+
+        GameAction redStrike = new RedStrikeAction();
+        Player player = Player.initializePlayer("testPlayer");
+        Board board = Board.buildBoard(1, 9);
+
+        assertTrue(redStrike.canExecute(board, player));
+        redStrike.execute(board, player);
+
+        assertEquals(board.getRedCoinCount(), 0);
+    }
+
+    @Test
+    public void testShouldIncreaseScoreByThreeForRedStrike() throws InvalidBoardException {
+
+        GameAction redStrike = new RedStrikeAction();
+        Player player = Player.initializePlayer("testPlayer");
+        Board board = Board.buildBoard(1, 9);
+
+        assertTrue(redStrike.canExecute(board, player));
+        redStrike.execute(board, player);
+
+        assertEquals(player.getScore(), 3);
     }
 
 }
